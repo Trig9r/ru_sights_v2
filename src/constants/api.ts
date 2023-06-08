22d35e09e171add30814b7ece388b00b5c60337f
Kirill Smirnov <smirnovkiryusha12@gmail.com>
2023-06-08 10:55:29 +0300

    fix: reference for webpurify

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,9 +1,6 @@
 const HTTP = 'http://';
 const HTTPS = 'https://';
 
-// cors-anywhere
-const CORS_ANYWHERE = 'cors-anywhere.herokuapp.com/';
-
 // webpurify
 const API_KEY_WEBPURIFY = '98edd69ac5345bc244cc82902644bd40';
 const WEBPURIFY = `api1.webpurify.com/services/rest/?api_key=${API_KEY_WEBPURIFY}&method=webpurify.live.check`;
@@ -13,5 +10,5 @@ export const WEBPURIFY_FORMAT = 'lang=ru&format=json';
 export const API_KEY_OPENCAGEDATA = 'a7c004a0c28846e3b4eb0cf2a6f73071';
 const OPENCAGEDATA = 'api.opencagedata.com/geocode/v1/json?q=';
 
-export const API_WEBPURIFY = HTTPS + CORS_ANYWHERE + HTTP + WEBPURIFY;
+export const API_WEBPURIFY = HTTPS + WEBPURIFY;
 export const API_OPENCAGEDATA = HTTPS + OPENCAGEDATA;

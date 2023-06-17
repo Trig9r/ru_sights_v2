f6decbec67c11ea9a34ea3a3c0e1b41c791c6242
Kirill Smirnov <smirnovkiryusha12@gmail.com>
2023-06-17 12:40:59 +0300

    <div className=App>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
+  Routes,
   RouterProvider,
 } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
@@ -25,27 +26,40 @@ import './App.css';
 
 const queryClient = new QueryClient();
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Header />}>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/города" element={<CitiesPage />} />
-      <Route path="/города/:name" element={<CityPage />} />
-      <Route path="/новаяДост" element={<AddSightPage />} />
-      <Route path="/достопримечательности/:type" element={<SightTypesPage />} />
-      <Route path="/aboutUs" element={<AboutUsPage />} />
-      <Route path="/избранное" element={<FavoritePage />} />
-      <Route path="/достопримечательность/:name" element={<SightPage />} />
-      <Route path="/достопримечательность/:name/изменить" element={<UpdateSightPage />} />
-    </Route>,
-  ),
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Header />}>
+//       <Route path="/" element={<HomePage />} />
+//       <Route path="/города" element={<CitiesPage />} />
+//       <Route path="/города/:name" element={<CityPage />} />
+//       <Route path="/новаяДост" element={<AddSightPage />} />
+//       <Route path="/достопримечательности/:type" element={<SightTypesPage />} />
+//       <Route path="/aboutUs" element={<AboutUsPage />} />
+//       <Route path="/избранное" element={<FavoritePage />} />
+//       <Route path="/достопримечательность/:name" element={<SightPage />} />
+//       <Route path="/достопримечательность/:name/изменить" element={<UpdateSightPage />} />
+//     </Route>,
+//   ),
+// );
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
-        <RouterProvider router={router} />
+        {/* <RouterProvider router={router} /> */}
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/города" element={<CitiesPage />} />
+            <Route path="/города/:name" element={<CityPage />} />
+            <Route path="/новаяДост" element={<AddSightPage />} />
+            <Route path="/достопримечательности/:type" element={<SightTypesPage />} />
+            <Route path="/aboutUs" element={<AboutUsPage />} />
+            <Route path="/избранное" element={<FavoritePage />} />
+            <Route path="/достопримечательность/:name" element={<SightPage />} />
+            <Route path="/достопримечательность/:name/изменить" element={<UpdateSightPage />} />
+          </Route>
+        </Routes>
       </div>
       <ReactQueryDevtools />
       <Analytics />
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 import '@/styles/index.css';
 import '@/styles/css/fonts.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
